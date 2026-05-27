51c94b836515a65ac7f9035e6ae527f44fc7d446
DarwinMelaya <melayadarwin@gmail.com>
2026-05-27 22:49:51 +0800

    Add admin routes and components to the application

--- frontend/src/Routers/Routers.jsx
+++ frontend/src/Routers/Routers.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home, SignUp, Login } from "../pages";
+import { Home, SignUp, Login, AdminDashboard, AdminReport } from "../pages";
 
 export const Routers = () => {
   return (
@@ -8,6 +8,8 @@ export const Routers = () => {
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        <Route path="/admin/report" element={<AdminReport />} />
       </Routes>
     </Router>
   );
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -3,3 +3,7 @@ export { default as Home } from "./home/Home";
 // Auth
 export { default as SignUp } from "./Auth/SignUp";
 export { default as Login } from "./Auth/Login";
+
+// Admin
+export { default as AdminDashboard } from "./Admin/AdminDashboard";
+export { default as AdminReport } from "./Admin/AdminReport";
